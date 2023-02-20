e8980f2f214c59fd310fbd7c93834cc95d4b421b
Bonface Masira <bonfacemasira@gmail.com>
2023-02-20 11:37:23 +0300

    d

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,10 +23,11 @@ int main(void)
 	if (lastofn > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastofn);
-	}else if (lastofn == 0)
+	}
+	else if (lastofn == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, lastofn);
-	}else
+	} else
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastofn);
 	}
