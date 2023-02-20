07fe05bcd3d5ce3bb13c43391c1189c29b630a76
Bonface Masira <bonfacemasira@gmail.com>
2023-02-20 11:32:37 +0300

    fix spacing error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,15 +22,15 @@ int main(void)
 
 	if (lastofn > 5)
 	{
-		printf ("Last digit of %d is %d and is greater than 5\n", n, lastofn);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastofn);
 	}
 	else if (lastofn == 0)
 	{
-		printf ("Last digit of %d is %d and is 0\n", n, lastofn);
+		printf("Last digit of %d is %d and is 0\n", n, lastofn);
 	}
 	else
 	 {
-		printf ("Last digit of %d is %d and is less than 6 and not 0\n", n, lastofn);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastofn);
 	}
 	return (0);
 }
