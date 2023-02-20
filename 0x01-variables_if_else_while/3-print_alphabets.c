517b8f2af18f6537470cf142c75cd0d283493176
Bonface Masira <bonfacemasira@gmail.com>
2023-02-20 15:04:03 +0300

    complete 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.io>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+char a;
+
+for (a = 'a'; a <= 'z'; a++)
+	putchar (a);
+for (a = 'Z'; a <= 'Z'; a++)
+	putchar (a);
+putchar ('\n');
+return (0);
+}
