88a226df7a30c7d945473a18ddefbef7446b3830
Bonface Masira <bonfacemasira@gmail.com>
2023-02-21 17:31:01 +0300

    attempt 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - main function
+ *
+ * @c: character to compare
+ *
+ * Return: 1 if 1, 0 if 0, -1 if -1
+ *
+ */
+
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	_putchar('-');
+	return (-1);
+}
