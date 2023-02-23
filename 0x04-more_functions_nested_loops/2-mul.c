dd78be68848c73fc8db6e48254b18a9b874a3808
Bonface Masira <bonfacemasira@gmail.com>
2023-02-23 14:58:10 +0300

    attempt 3

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -7,6 +7,7 @@
   *
   * Return: The result of multiplication between x param and y param
   */
+ 
 int mul(int a, int b)
 {
 	return (a * b);
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @a: The first number to multiply
+ * @b: The second number to multiply with first number
+ *
+ * Return: The result of multiplication between x param and y param
+ */
+
+void print_numbers(void)
+{
+    int x;
+
+    for (x = 0; x <= 9; x++)
+    {
+        _putchar('x');
+    }
+    _putchar('\n');
+    
+}
