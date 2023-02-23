27ee9df58b0ef197632c2eea2850bb2d1d1dfb78
Bonface Masira <bonfacemasira@gmail.com>
2023-02-23 14:42:17 +0300

    final attempt at 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,12 +2,12 @@
 
 /**
   * mul - Multiplies two integers
-  * @x: The first number to multiply
-  * @y: The second number to multiply with first number
+  * @a: The first number to multiply
+  * @b: The second number to multiply with first number
   *
   * Return: The result of multiplication between x param and y param
   */
-int mul(int x, int y)
+int mul(int a, int b)
 {
-	return (x * y);
+	return (a * b);
 }
