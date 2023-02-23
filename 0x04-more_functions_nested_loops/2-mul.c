d0bd93b4af97593fac686b9abfc47b9fe8065664
Bonface Masira <bonfacemasira@gmail.com>
2023-02-23 14:38:54 +0300

    define Betty

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -5,7 +5,7 @@
  * @a: The first number to multiply
  * @b: The second number to multiply with first number
  *
- * Return: The result of multiplication between x param and y param
+ * Return: The result of multiplication between a param and b param
  */
 
 int mul(int a, int b)
