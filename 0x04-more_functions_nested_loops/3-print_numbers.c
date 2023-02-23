e995b584edddccb54e81eee5bc0ccad066f7b1f4
Bonface Masira <bonfacemasira@gmail.com>
2023-02-23 15:02:30 +0300

    final attempt at 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,14 +5,14 @@
   *
   * Return: The numbers since 0 up to 9
   */
-
 void print_numbers(void)
 {
-    int x;
+	int x;
 
-    for (x = 0; x <= 9; x++)
-    {
-        _putchar(x + '0');
-    }
-    _putchar('\n');
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar(x + '0');
+	}
+
+	_putchar('\n');
 }
