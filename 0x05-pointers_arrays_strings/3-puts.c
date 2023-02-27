897d9b84cbd2a41b320e0358fefa2b633f8146c4
Bonface Masira <bonfacemasira@gmail.com>
2023-02-27 17:03:44 +0300

    complete 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,18 @@
 #include "main.h"
+
+/**
+  * _puts - Prints a string, followed by a new line, to stdout
+  * @s: String to print
+  *
+  * Return: void
+  */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+
+	_putchar('\n');
+}
